aee116ec03526286e2e98c11282ba3cf211b2a1d
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-02-28 20:48:55 -0600

    Add ship factory

--- src/factories.js
+++ src/factories.js
@@ -1,11 +1,15 @@
 const ship = (length) => {
     let hits = 0;
+    const shipLength = length;
 
     const getsHit = () => {
-
         hits += 1;
-        
     }
 
+    const isSunk = () => {
+        return hits >= shipLength;
+    }
+
+    return { shipLength, getsHit, isSunk }
 
 }
